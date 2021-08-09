9a91a7ec4fea5b9c2b34ce5de63e473701f83fd3
LJason <ljason@ljason.cn>
2021-08-09 20:24:43 +0800

    add menu
    Signed-off-by: LJason <ljason@ljason.cn>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,17 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, Menu, Submenu};
+
+pub fn get() -> Menu {
+  let add = CustomMenuItem::new("add".to_string(), "Add");
+  let file = Submenu::new("File".to_string(), Menu::new().add_item(add));
+  Menu::new().add_submenu(file)
+}
+
 fn main() {
   tauri::Builder::default()
+    .menu(get())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
